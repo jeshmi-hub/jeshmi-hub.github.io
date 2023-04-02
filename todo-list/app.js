98763dbb7493086cc6c95dcbaf56ac6e7461ba64
jeshmi-hub <rajakjeshmi19@gmail.com>
2023-04-02 19:19:53 +0545

    added title field in todo

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,21 +1,47 @@
 const inputValue = document.getElementsByClassName('inputValue')[0]
+const titleValue = document.getElementsByClassName('title')[0]
 const addTaskBtn = document.getElementsByClassName('btn')[0]
 
 addTaskBtn.addEventListener('click', ()=>{
-    if(inputValue.value.trim() !=0){
+    if(inputValue.value.trim() !=0 || titleValue.value.trim() !=0){
     let localItems = JSON.parse(localStorage.getItem('localItem'))
     if(localItems === null){
-        taskList = []
+        titleList = []
+        taskList = [] 
     }else{
+        titleList = localItems;
         taskList = localItems;
     }
     taskList.push(inputValue.value)
+    titleList.push(titleValue.value)
     localStorage.setItem('localItem', JSON.stringify(taskList))
-
+    localStorage.setItem('localItem', JSON.stringify(titleList))
     }
     
+    showTitle()
     showList()
 })
+
+
+function showTitle(){
+    let outPut = '';
+    let taskTitleShow = document.querySelector('.todoTitle')
+    let localItems = JSON.parse(localStorage.getItem('localItem'))
+    if(localItems === null){
+        titleList = []
+    }else{
+        titleList = localItems;
+    }
+    titleList.forEach((data,index)=>{
+        outPut += `
+        <h1>${data}
+        <button class="del" onclick="deleteItem(${index})"><i class="fa-solid fa-trash delete(${index})"></i></button>
+        </h1>`
+    });
+
+    taskTitleShow.innerHTML = outPut;
+}
+
 
 function showList(){
     let outPut = '';
@@ -34,19 +60,25 @@ function showList(){
         </li>`
 
     });
-    taskListShow.innerHTML = outPut;
+    
+    taskListShow.innerHTML = outPut;  
 }
 
+showTitle()
 showList()
 
 function deleteItem(index){
     let localItems = JSON.parse(localStorage.getItem('localItem'))
     taskList.splice(index, 1)
+    titleList.splice(index,1)
     localStorage.setItem('localItem', JSON.stringify(taskList))
+    localStorage.setItem('localItem', JSON.stringify(titleList))
+    showTitle()
     showList()
 }
 
 function clearTask(){
     localStorage.clear()
+    showTitle()
     showList()
 }
